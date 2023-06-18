bbf6166c4fc66f7255ff716eac5308b8a40d0d43
zhouzengming <1548602509@qq.com>
2023-06-18 14:04:51 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,27 @@
-#echo "sleeping..."
-#sleep 5
-#echo "merging..."
-#python3 ./1_merger/1_merge_news.py
-#sleep 5
-#echo "picking sample..."
-#python3 ./2_modeler/0_pick_sample.py
-##sleep 5
-#echo "modelling and checking..."
+# /bin/bash
+echo "starting spyder laucher..."
+sleep 5
+python3 ./0_spyder/main_laucher.py
+echo "spyder finished!"
+sleep 5
+
+echo "start merging..."
+sleep 5
+python3 ./1_merger/1_merge_news.py
+echo "merge finished!"
+sleep 5
+
+echo "start picking sample..."
+sleep 5
+python3 ./2_modeler/0_pick_sample.py
+echo "sample generated!"
+sleep 5
+
+echo "modelling and checking..."
+sleep 5
 python3 ./2_modeler/1_ldamodeling.py
-#sleep 5
-#echo "comparing..."
-#python3 ./3_checker/0_model_compare.py
+sleep 5
+
+echo "comparing..."
+sleep 5
+python3 ./3_checker/0_model_compare.py
